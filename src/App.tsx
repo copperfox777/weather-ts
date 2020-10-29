78d2f09e9771cef55a84870a62123afe18ba4605
copperfox <copperfox@mail.ru>
2020-10-29 22:54:24 +0300

    app.css updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './app.css';
+import './App.css';
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom"
 
 import Home from './pages/home'
