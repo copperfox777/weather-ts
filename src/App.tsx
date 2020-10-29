4e35cbdf59ce9358a4ef9ec9a2adfa5fdcb02f6a
copperfox <copperfox@mail.ru>
2020-10-29 23:01:39 +0300

    CI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom"
+import './App.css';
 
 import Home from './pages/home'
 import History from './pages/history'
